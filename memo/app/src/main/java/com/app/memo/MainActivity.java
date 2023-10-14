62590f1f09485be85e34f39494e837e2009debf4
ALEXIS CARBILLET <alexiscarbillet@users.noreply.github.com>
2023-10-13 23:15:34 -0400

    add weight conversion

--- memo/app/src/main/java/com/app/memo/MainActivity.java
+++ memo/app/src/main/java/com/app/memo/MainActivity.java
@@ -2,6 +2,7 @@ package com.app.memo;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -11,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     private EditText inputEditText;
     private TextView resultTextView;
-    private Spinner fromSpinner, toSpinner;
+    private Spinner fromSpinner, toSpinner, categorySpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,41 +23,76 @@ public class MainActivity extends AppCompatActivity {
         resultTextView = findViewById(R.id.resultTextView);
         fromSpinner = findViewById(R.id.fromSpinner);
         toSpinner = findViewById(R.id.toSpinner);
+        categorySpinner = findViewById(R.id.categorySpinner);
 
         // Define unit options
-        String[] units = {"Meter", "Foot", "Inch"};
+        String[] lengthUnits = {"Meter", "Foot", "Inch"};
+        String[] weightUnits = {"Kilogram", "Gram", "Pound"};
+        String[] categories = {"Length", "Weight"};
 
         // Create adapters for spinners
-        ArrayAdapter<String> unitAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, units);
-        unitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> lengthAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, lengthUnits);
+        lengthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        // Set adapters for spinners
-        fromSpinner.setAdapter(unitAdapter);
-        toSpinner.setAdapter(unitAdapter);
+        ArrayAdapter<String> weightAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, weightUnits);
+        weightAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Set adapters for the spinners
+        categorySpinner.setAdapter(categoryAdapter);
+        fromSpinner.setAdapter(lengthAdapter);
+        toSpinner.setAdapter(lengthAdapter);
+
+        // Set an OnItemSelectedListener for the categorySpinner
+        categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                updateUnitSpinners();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Do nothing here
+            }
+        });
+    }
+
+    private void updateUnitSpinners() {
+        String selectedCategory = categorySpinner.getSelectedItem().toString();
+
+        if (selectedCategory.equals("Length")) {
+            fromSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new String[]{"Meter", "Foot", "Inch"}));
+            toSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new String[]{"Meter", "Foot", "Inch"}));
+        } else if (selectedCategory.equals("Weight")) {
+            fromSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new String[]{"Kilogram", "Gram", "Pound"}));
+            toSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new String[]{"Kilogram", "Gram", "Pound"}));
+        }
     }
 
     public void convert(View view) {
         double inputValue = Double.parseDouble(inputEditText.getText().toString());
         String fromUnit = fromSpinner.getSelectedItem().toString();
         String toUnit = toSpinner.getSelectedItem().toString();
-        double result = convertLength(inputValue, fromUnit, toUnit);
+        double result = convert(inputValue, fromUnit, toUnit);
         resultTextView.setText(String.valueOf(result));
     }
 
-    private double convertLength(double value, String fromUnit, String toUnit) {
-        // Conversion logic
-        if (fromUnit.equals("Meter") && toUnit.equals("Foot")) {
-            return value * 3.28084;
-        } else if (fromUnit.equals("Meter") && toUnit.equals("Inch")) {
-            return value * 39.3701;
-        } else if (fromUnit.equals("Foot") && toUnit.equals("Meter")) {
-            return value * 0.3048;
-        } else if (fromUnit.equals("Foot") && toUnit.equals("Inch")) {
-            return value * 12.0;
-        } else if (fromUnit.equals("Inch") && toUnit.equals("Meter")) {
-            return value * 0.0254;
-        } else if (fromUnit.equals("Inch") && toUnit.equals("Foot")) {
-            return value * 0.0833333;
+    private double convert(double value, String fromUnit, String toUnit) {
+        // Conversion logic for length
+        if ((fromUnit.equals("Meter") || fromUnit.equals("Kilogram")) && (toUnit.equals("Foot") || toUnit.equals("Pound"))) {
+            return (fromUnit.equals("Meter") ? value * 3.28084 : value * 2.20462);
+        } else if ((fromUnit.equals("Meter") || fromUnit.equals("Kilogram")) && (toUnit.equals("Inch") || toUnit.equals("Gram"))) {
+            return (fromUnit.equals("Meter") ? value * 39.3701 : value * 1000.0);
+        } else if ((fromUnit.equals("Foot") || fromUnit.equals("Pound")) && (toUnit.equals("Meter") || toUnit.equals("Kilogram"))) {
+            return (fromUnit.equals("Foot") ? value * 0.3048 : value * 0.453592);
+        } else if ((fromUnit.equals("Foot") || fromUnit.equals("Pound")) && (toUnit.equals("Inch") || toUnit.equals("Gram"))) {
+            return (fromUnit.equals("Foot") ? value * 12.0 : value * 453.592);
+        } else if ((fromUnit.equals("Inch") || fromUnit.equals("Gram")) && (toUnit.equals("Meter") || toUnit.equals("Kilogram"))) {
+            return (fromUnit.equals("Inch") ? value * 0.0254 : value * 0.001);
+        } else if ((fromUnit.equals("Inch") || fromUnit.equals("Gram")) && (toUnit.equals("Foot") || toUnit.equals("Pound"))) {
+            return (fromUnit.equals("Inch") ? value * 0.00220462 : value * 0.00220462);
         } else {
             return value;
         }
