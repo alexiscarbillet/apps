e8907030542edc656d12b71d3aa32397fba2e6d3
ALEXIS CARBILLET <alexiscarbillet@users.noreply.github.com>
2023-10-13 23:21:54 -0400

    add volume and temperature conversion

--- memo/app/src/main/java/com/app/memo/MainActivity.java
+++ memo/app/src/main/java/com/app/memo/MainActivity.java
@@ -28,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
         // Define unit options
         String[] lengthUnits = {"Meter", "Foot", "Inch"};
         String[] weightUnits = {"Kilogram", "Gram", "Pound"};
-        String[] categories = {"Length", "Weight"};
+        String[] temperatureUnits = {"Celsius", "Fahrenheit", "Kelvin"};
+        String[] volumeUnits = {"Liter", "Milliliter", "Gallon"};
 
         // Create adapters for spinners
         ArrayAdapter<String> lengthAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, lengthUnits);
@@ -37,11 +38,14 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> weightAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, weightUnits);
         weightAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
-        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> temperatureAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, temperatureUnits);
+        temperatureAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<String> volumeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, volumeUnits);
+        volumeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         // Set adapters for the spinners
-        categorySpinner.setAdapter(categoryAdapter);
+        categorySpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new String[]{"Length", "Weight", "Temperature", "Volume"}));
         fromSpinner.setAdapter(lengthAdapter);
         toSpinner.setAdapter(lengthAdapter);
 
@@ -68,6 +72,12 @@ public class MainActivity extends AppCompatActivity {
         } else if (selectedCategory.equals("Weight")) {
             fromSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new String[]{"Kilogram", "Gram", "Pound"}));
             toSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new String[]{"Kilogram", "Gram", "Pound"}));
+        } else if (selectedCategory.equals("Temperature")) {
+            fromSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new String[]{"Celsius", "Fahrenheit", "Kelvin"}));
+            toSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new String[]{"Celsius", "Fahrenheit", "Kelvin"}));
+        } else if (selectedCategory.equals("Volume")) {
+            fromSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new String[]{"Liter", "Milliliter", "Gallon"}));
+            toSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new String[]{"Liter", "Milliliter", "Gallon"}));
         }
     }
 
@@ -80,7 +90,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private double convert(double value, String fromUnit, String toUnit) {
-        // Conversion logic for length
+        if (fromUnit.equals(toUnit) || value == 0) {
+            return value; // No conversion needed
+        }
+
         if ((fromUnit.equals("Meter") || fromUnit.equals("Kilogram")) && (toUnit.equals("Foot") || toUnit.equals("Pound"))) {
             return (fromUnit.equals("Meter") ? value * 3.28084 : value * 2.20462);
         } else if ((fromUnit.equals("Meter") || fromUnit.equals("Kilogram")) && (toUnit.equals("Inch") || toUnit.equals("Gram"))) {
@@ -93,9 +106,50 @@ public class MainActivity extends AppCompatActivity {
             return (fromUnit.equals("Inch") ? value * 0.0254 : value * 0.001);
         } else if ((fromUnit.equals("Inch") || fromUnit.equals("Gram")) && (toUnit.equals("Foot") || toUnit.equals("Pound"))) {
             return (fromUnit.equals("Inch") ? value * 0.00220462 : value * 0.00220462);
+        } else if ((fromUnit.equals("Celsius") || fromUnit.equals("Fahrenheit") || fromUnit.equals("Kelvin")) &&
+                (toUnit.equals("Celsius") || toUnit.equals("Fahrenheit") || toUnit.equals("Kelvin")) && fromUnit != toUnit) {
+            return convertTemperature(value, fromUnit, toUnit);
+        } else if ((fromUnit.equals("Liter") || fromUnit.equals("Milliliter") || fromUnit.equals("Gallon")) &&
+                (toUnit.equals("Liter") || toUnit.equals("Milliliter") || toUnit.equals("Gallon")) && fromUnit != toUnit) {
+            return convertVolume(value, fromUnit, toUnit);
+        } else {
+            return value;
+        }
+    }
+
+    private double convertTemperature(double value, String fromUnit, String toUnit) {
+        if (fromUnit.equals("Celsius") && toUnit.equals("Fahrenheit")) {
+            return (value * 9/5) + 32;
+        } else if (fromUnit.equals("Celsius") && toUnit.equals("Kelvin")) {
+            return value + 273.15;
+        } else if (fromUnit.equals("Fahrenheit") && toUnit.equals("Celsius")) {
+            return (value - 32) * 5/9;
+        } else if (fromUnit.equals("Fahrenheit") && toUnit.equals("Kelvin")) {
+            return (value - 32) * 5/9 + 273.15;
+        } else if (fromUnit.equals("Kelvin") && toUnit.equals("Celsius")) {
+            return value - 273.15;
+        } else if (fromUnit.equals("Kelvin") && toUnit.equals("Fahrenheit")) {
+            return (value - 273.15) * 9/5 + 32;
+        } else {
+            return value;
+        }
+    }
+
+    private double convertVolume(double value, String fromUnit, String toUnit) {
+        if (fromUnit.equals("Liter") && toUnit.equals("Milliliter")) {
+            return value * 1000;
+        } else if (fromUnit.equals("Liter") && toUnit.equals("Gallon")) {
+            return value * 0.264172;
+        } else if (fromUnit.equals("Milliliter") && toUnit.equals("Liter")) {
+            return value * 0.001;
+        } else if (fromUnit.equals("Milliliter") && toUnit.equals("Gallon")) {
+            return value * 0.000264172;
+        } else if (fromUnit.equals("Gallon") && toUnit.equals("Liter")) {
+            return value * 3.78541;
+        } else if (fromUnit.equals("Gallon") && toUnit.equals("Milliliter")) {
+            return value * 3785.41;
         } else {
             return value;
         }
     }
 }
-
